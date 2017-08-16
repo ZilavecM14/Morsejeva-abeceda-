944d0a3139551fe2f2b240d8b33a48e360c3d8f1
ZilavecM14 <mojca.zilavec@gmail.com>
2017-08-16 14:20:59 +0200

    Update Morsejeva abeceda konec.py
    Novo zadnje

--- Morsejeva abeceda konec.py
+++ Morsejeva abeceda konec.py
@@ -8,70 +8,70 @@ class Morsejeva():
     def __init__(self,master):
 
 
-        naslovLabel=Label(master,text="Morsejeva abeceda",font=("Arial",24,"bold"),fg="red", justify=CENTER)  
+        naslovLabel = Label(master,text="Morsejeva abeceda",font=("Arial",24,"bold"),fg="red", justify=CENTER)  
         naslovLabel.grid(row=1,column=1,columnspan=3,pady=10,padx=20) 
 
-        crkaLabel=Label(master,text="Crka:",fg="blue",bg="deep pink", font=("Arial",18))
+        crkaLabel = Label(master,text="Crka:",fg="blue",bg="deep pink", font=("Arial",18))
         crkaLabel.grid(row=2,column=1)
         
         crka = ""
-        self.crkaEntry=Entry(master,width=20,bd=5, textvariable = crka)
+        self.crkaEntry = Entry(master,width=20,bd=5, textvariable = crka)
         self.crkaEntry.grid(row=2,column=2)
         
-        znakLabel=Label(master,text="Znak:",fg="red",bg="purple", font=("Arial",18))
+        znakLabel = Label(master,text="Znak:",fg="red",bg="purple", font=("Arial",18))
         znakLabel.grid(row=3,column=1)
 
         znak = ""
-        self.znakEntry=Entry(master,width=20,bd=5, textvariable = znak)
+        self.znakEntry = Entry(master,width=20,bd=5, textvariable = znak)
         self.znakEntry.grid(row=3,column=2)
 
-        pretvoriButton=Button(master,text="Pretvori", bg="purple1",font=("Arial",12,"bold"), bd=5,justify=CENTER, command= self.pretvori) 
+        pretvoriButton = Button(master,text="Pretvori", bg="purple1",font=("Arial",12,"bold"), bd=5,justify=CENTER, command= self.pretvori) 
         pretvoriButton.grid(row=6,column=2,columnspan=2,pady=20,padx=20)
 
-        osveziButton=Button(master,text="Osvezi", bg="deep pink",font=("Arial",12,"bold"),bd=5,justify=CENTER, command=self.osvezi)
+        osveziButton = Button(master,text="Osvezi", bg="deep pink",font=("Arial",12,"bold"),bd=5,justify=CENTER, command=self.osvezi)
         osveziButton.grid(row=6,column=3,columnspan=2,pady=20,padx=20)
 
-        shraniButton=Button(master,text="Shrani", bg="red",font=("Arial",12,"bold"),bd=5,justify=CENTER, command=self.shrani)
+        shraniButton = Button(master,text="Shrani", bg="red",font=("Arial",12,"bold"),bd=5,justify=CENTER, command=self.shrani)
         shraniButton.grid(row=6,column=8,columnspan=2,pady=20,padx=20)
 
-        naslovLabel=Label(master,text="Besedilo:",font=("Arial",18),bg="red")
+        naslovLabel = Label(master,text="Besedilo:",font=("Arial",18),bg="red")
         naslovLabel.grid(row=2, column=6, pady=20,padx=20)
 
-        self.besediloLabel=Label(master,text="", font=("Arial",14)) 
+        self.besediloLabel = Label(master,text="", font=("Arial",14)) 
         self.besediloLabel.grid(row=2,column=7)
 
-        self.besedilo=""
+        self.besedilo = ""
 
-        pocistiButton=Button(master,text="Pocisti",bg="blue", font=("Arial",12,"bold"),bd=5,justify=CENTER,command=self.pocisti)
+        pocistiButton = Button(master,text="Pocisti",bg="blue", font=("Arial",12,"bold"),bd=5,justify=CENTER,command=self.pocisti)
         pocistiButton.grid(row=6,column=7,pady=20,padx=20)
                              
     def pretvori(self):  
-        crka=self.crkaEntry.get()
-        crka=str(crka).upper()  
-        znak=self.znakEntry.get()
+        crka = self.crkaEntry.get()
+        crka = str(crka).upper()  
+        znak = self.znakEntry.get()
 
-        if crka!="":    
+        if crka != "":    
             for i in crka:   
-                indeks=Abeceda.index(i) 
-                mors=Morse[indeks] 
-                self.besedilo+=mors+'|' 
-            self.besedilo=self.besedilo[0:len(self.besedilo)-1]  
+                indeks = Abeceda.index(i) 
+                mors = Morse[indeks] 
+                self.besedilo += mors + '|' 
+            self.besedilo = self.besedilo[0:len(self.besedilo)-1]  
             self.znakEntry.insert(0,self.besedilo) 
-            self.besediloLabel["text"]=(self.besedilo)  
+            self.besediloLabel["text"] = (self.besedilo)  
             
 
-        elif znak!="": 
-            x=znak.split('|')
+        elif znak != "": 
+            x = znak.split('|')
             for i in x:
-                indek=Morse.index(i)
-                crk=Abeceda[indek]
-                self.besedilo+=crk
+                indek = Morse.index(i)
+                crk = Abeceda[indek]
+                self.besedilo += crk
             self.crkaEntry.insert(0,self.besedilo)
-            self.besediloLabel["text"]=(self.besedilo)
+            self.besediloLabel["text"] = (self.besedilo)
 
     def shrani(self): 
-        zac_besedilo=self.crkaEntry.get()
-        prev_besedilo=self.besedilo
+        zac_besedilo = self.crkaEntry.get()
+        prev_besedilo = self.besedilo
         with open ("Prevod.txt", "wt", encoding="utf8") as f:
             f.write("Zacetno besedilo:"+"\n")
             for x in zac_besedilo:
@@ -89,10 +89,10 @@ class Morsejeva():
 
     def pocisti(self):
         self.osvezi() 
-        self.besedilo="" 
-        self.besediloLabel["text"]=("") 
+        self.besedilo = "" 
+        self.besediloLabel["text"] = ("") 
             
-            
-root=Tk() 
-app=Morsejeva(root)
-root.mainloop()
+root = Tk() 
+app = Morsejeva(root)
+root.mainloop() 
+        
